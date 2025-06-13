16049267011b3bbd0a954df7e3151384c6a20083
mrekk-hub <maxim.shlyahtin@yandex.ru>
2025-06-13 14:04:30 +0300

    Добавил заглушку противника и террейн

---
+++ Assets/Scripts/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ConsoleLogger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        Debug.Log("This is start!");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
